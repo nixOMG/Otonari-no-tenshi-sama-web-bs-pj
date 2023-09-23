f285612a0d6b183ab2e120a42c39414b772a9a96
nixOMG <tanlmht859a@gmail.com>
2023-09-23 20:11:18 +0700

    The frontend of the website
    Still need to be fixed

---
+++ script.js
@@ -0,0 +1,53 @@
+var swiper = new Swiper(".popular-content", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 755500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    280: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    510: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    758: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    758: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+});
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
